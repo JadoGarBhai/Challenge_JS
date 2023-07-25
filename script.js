f06f685cc8937db1d9feabe6097a22f29a1fabd7
JadoGarBhai <sikandernawaz35@gmail.com>
2023-07-25 23:28:57 +0500

    case_1

---
+++ script.js
@@ -0,0 +1,35 @@
+// Case No: 01
+
+// input = 1.
+// output = One.
+
+// SOLUTION:
+
+function case1(n) {
+    switch (n) {
+        case 0:
+            return "zero";
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+        case 10:
+            return "ten";
+    }
+};
+
+console.log(case1(9));
