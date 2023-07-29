f22ebb793129e9357f45e945f989c477c8f1d75a
JadoGarBhai <sikandernawaz35@gmail.com>
2023-07-29 17:00:25 +0500

    successfully_fuck_this_problem

--- script.js
+++ script.js
@@ -6,61 +6,63 @@
 // SOLUTION:
 
 function case1(n) {
-    switch (n) {
-        case 0:
-            return "zero";
-        case 1:
-            return "one";
-        case 2:
-            return "two";
-        case 3:
-            return "three";
-        case 4:
-            return "four";
-        case 5:
-            return "five";
-        case 6:
-            return "six";
-        case 7:
-            return "seven";
-        case 8:
-            return "eight";
-        case 9:
-            return "nine";
-    }
+  switch (n) {
+    case 0:
+      return "zero";
+    case 1:
+      return "one";
+    case 2:
+      return "two";
+    case 3:
+      return "three";
+    case 4:
+      return "four";
+    case 5:
+      return "five";
+    case 6:
+      return "six";
+    case 7:
+      return "seven";
+    case 8:
+      return "eight";
+    case 9:
+      return "nine";
+  }
 };
 
 function case2(n) {
-    switch (n) {
-        case 11:
-            return "eleven";
-        case 12:
-            return "tevele";
-        case 13:
-            return "thirteen";
-        case 14:
-            return "fourteen";
-        case 15:
-            return "fifteen";
-        case 66:
-            return "sixteen";
-        case 17:
-            return "seventeen";
-        case 18:
-            return "eighteen";
-        case 19:
-            return "nineteen";
-    }
+  switch (n) {
+    case 10:
+        return "ten"
+    case 11:
+      return "eleven";
+    case 12:
+      return "tevele";
+    case 13:
+      return "thirteen";
+    case 14:
+      return "fourteen";
+    case 15:
+      return "fifteen";
+    case 66:
+      return "sixteen";
+    case 17:
+      return "seventeen";
+    case 18:
+      return "eighteen";
+    case 19:
+      return "nineteen";
+    case 20:
+        return "twenty"
+  }
 };
 
 function case3(n) {
-    if (n.length === 3) {
+    if (n >= 100 && n <= 999) {
         let a = n.toString();
-        for (let index = 0; index < a.length; index++) {
-            let b = case1(a.charAt(0)) + "hundred" + case2(a.substring(1));
-            return b;
-        }
+        let result = `${case1(parseInt(a.charAt(0)))} hundered ${case2(parseInt(a.substring(1)))}`;
+        return result;
     }
-}
+};
 
-console.log(case3(115));
+console.log(case3(117));
